439e6674a8a22ec72abb90aafa929bf2bd9f6e79
Andres Parra <andeveling@gmail.com>
2022-10-20 11:43:50 -0500

    add-cover-image

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,22 +1,20 @@
-import React from 'react';
-import { Link } from 'react-scroll';
+import React from "react"
 
 const Project = ({ item }) => {
   return (
-    <div key={item.id} className='flex flex-col bg-slate-400 bg-opacity-10 rounded-2xl p-5 hover:scale-110 duration-300 items-center text-center'>
+    <div
+      key={item.id}
+      className='flex flex-col bg-slate-400 bg-opacity-10 rounded-2xl p-5 hover:scale-110 duration-300 items-center text-center'>
       <div className=' h-fit  '>
-        <a href={item.url} target={'_blank'} >
-
-          <img className='rounded-2xl h-80 cursor-pointer ' src={item.image} alt='' />
+        <a href={item.url} target={"_blank"} rel='noreferrer'>
+          <img className='rounded-2xl h-80 cursor-pointer object-cover' src={item.image} alt='' />
         </a>
       </div>
       <p className='capitalize text-amber-500 text-sm mb-3'>{item.category}</p>
       <h3 className='text-2xl font-semibold capitalize mb-3'>{item.name}</h3>
-      <p className='text-base text-justify max-w-md'>
-        {item.description}
-      </p>
+      <p className='text-base text-justify max-w-md'>{item.description}</p>
     </div>
-  );
-};
+  )
+}
 
-export default Project;
+export default Project
